9ea52a369005924d4b1f041224047e2cd611c06c
abisheksivaraj2001@gmail.com <abisheksivaraj2001@gmail.com>
2023-09-26 15:43:22 +0530

    contact

---
+++ script.js
@@ -0,0 +1,19 @@
+"use script";
+
+const nameEl = document.querySelector(".input-name");
+
+const mailEl = document.querySelector(".input-mail");
+
+const subjectEl = document.querySelector(".input-subject");
+
+const messageEl = document.querySelector(".input-message");
+
+const resetEl = document.querySelector(".reset");
+
+resetEl.addEventListener("click", () => {
+  nameEl.value = "";
+  mailEl.value ='';
+  subjectEl.value ='';
+  messageEl.value ='';
+});
+
